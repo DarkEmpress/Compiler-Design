6b1ddc7c5acca42f15059542e18fb867b0289117
DarkEmpress <snehababy0212@gmail.com>
2024-01-23 20:40:07 +0530

    even01s
    a DFA for even 0s and 1s

---
+++ lexicalanalyser.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+
+int main()
+{
+    char lang[100];
+    printf("enter your expression: ");
+    scanf("%s",&lang);
+    //check criteria for various elements of the language (tokenisation)
+
+    return 0;
+}
